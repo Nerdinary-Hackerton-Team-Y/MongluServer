b6b7bee11b8a73a0e2e3198ac0db81f96e7b1422
wingyou <winwing.you@gmail.com>
2024-11-24 01:09:56 +0900

    feat: 엔터티 및 스키마 수정 병합

--- schema.sql
+++ schema.sql
@@ -2,98 +2,12 @@ DROP DATABASE IF EXISTS mongle;
 CREATE DATABASE IF NOT EXISTS mongle;
 USE mongle;
 
-<<<<<<< HEAD
-CREATE TABLE `User` (
-  `id` int,
-=======
 CREATE TABLE `user` (
   `id` int AUTO_INCREMENT,
->>>>>>> main
   `name` varchar(100),
   `username` varchar(100),
   `nickname` varchar(100),
   `password` varchar(100),
-<<<<<<< HEAD
-  `profilePictureUrl` varchar(20),
-  `status` enum('activated', 'deactivated'),
-  `joinedAt` datetime,
-  `updatedAt` datetime,
-  `deletedAt` datetime,
-  PRIMARY KEY (`id`)
-);
-
-CREATE TABLE `Quest` (
-  `id` int,
-  `postId` int,
-  `title` varchar(100),
-  `status` enum('activated', 'deactivated'),
-  `createdAt` datetime,
-  `updatedAt` datetime,
-  `deletedAt` datetime,
-  PRIMARY KEY (`id`)
-);
-
-CREATE TABLE `Post` (
-  `id` int,
-  `userId` int,
-  `questId` int,
-  `imageUrl` varchar(100),
-  `content` varchar(100),
-  `isQuest` int,
-  `status` enum('activated', 'deactivated'),
-  `createdAt` datetime,
-  `updatedAt` datetime,
-  `deletedAt` datetime,
-  PRIMARY KEY (`id`),
-  FOREIGN KEY (`userId`) REFERENCES `User`(`id`),
-  FOREIGN KEY (`questId`) REFERENCES `Quest`(`id`)
-);
-
-CREATE TABLE `Comment` (
-  `id` int,
-  `userId` int,
-  `postId` int,
-  `content` varchar(100),
-  `status` enum('activated', 'deactivated'),
-  `createdAt` datetime,
-  `updatedAt` datetime,
-  `deletedAt` datetime,
-  PRIMARY KEY (`id`),
-  FOREIGN KEY (`userId`) REFERENCES `User`(`id`),
-  FOREIGN KEY (`postId`) REFERENCES `Post`(`id`)
-);
-
-CREATE TABLE `Like` (
-  `id` int,
-  `postId` int,
-  `userId` int,
-  `status` enum('activated', 'deactivated'),
-  `createdAt` datetime,
-  `updatedAt` datetime,
-  `deletedAt` datetime,
-  PRIMARY KEY (`id`),
-  FOREIGN KEY (`userId`) REFERENCES `User`(`id`),
-  FOREIGN KEY (`postId`) REFERENCES `Post`(`id`)
-);
-
-CREATE TABLE `Hashtag` (
-  `id` int,
-  `tag` varchar(100),
-  `status` enum('activated', 'deactivated'),
-  `createdAt` datetime,
-  `updatedAt` datetime,
-  `deletedAt` datetime,
-  PRIMARY KEY (`id`)
-);
-
-CREATE TABLE `PostHashtag` (
-  `id` int,
-  `postId` int,
-  `hashtagId` int,
-  PRIMARY KEY (`id`),
-  FOREIGN KEY (`postId`) REFERENCES `Post`(`id`),
-  FOREIGN KEY (`hashtagId`) REFERENCES `Hashtag`(`id`)
-=======
   `profile_picture_url` varchar(20),
   `status` VARCHAR(15) DEFAULT 'ACTIVATED',
   `joined_at` datetime,
@@ -177,6 +91,5 @@ CREATE TABLE `post_hashtag` (
   PRIMARY KEY (`id`),
   FOREIGN KEY (`post_id`) REFERENCES `post`(`id`),
   FOREIGN KEY (`hashtag_id`) REFERENCES `hashtag`(`id`)
->>>>>>> main
 );
 
