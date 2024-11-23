e474fb58f40cfa3d1d7061feb70fea84536caba2
wingyou <winwing.you@gmail.com>
2024-11-24 01:30:42 +0900

    feat: startup 스크립트가 환경변수를 로드하도록 설정

--- scripts/deploy/startup.sh
+++ scripts/deploy/startup.sh
@@ -8,6 +8,8 @@ if [ -n "$pid" ]; then
   echo "Error: PID '${pid}' is conneted to port 80. cannot startup new process."
   exit 1
 fi
+
+source ~/.env
 nohup sudo java -jar server.jar --spring.profiles.active=dev > out.log 2>&1 & disown
 echo "server started at port 80."
 echo "stdout & stderr directed to 'out.log'"
