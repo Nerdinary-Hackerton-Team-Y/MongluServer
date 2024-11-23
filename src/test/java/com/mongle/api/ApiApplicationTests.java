da5ca36185eb642dceb4705c9080cede2cd81b9f
wingyou <winwing.you@gmail.com>
2024-11-23 14:50:52 +0900

    first commit

---
+++ src/test/java/com/mongle/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mongle.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
